01f7ac48bed6d5186634b16e5fe1f08bb0ce6fff
GZS <guozhaosengzs@yahoo.com>
2022-05-30 15:06:24 -0400

    updated

--- bill_classify.py
+++ bill_classify.py
@@ -2,39 +2,44 @@ import pandas as pd
 import os.path
 from classification import CrossEncoderEventExtractor
 from pprint import pprint
-records_df = pd.read_csv('bill_assm.csv')
 
-df_classification = records_df.copy()
-df_classification.drop_duplicates(inplace=True, ignore_index=True)
+def main():
+    records_df = pd.read_csv('bill_assm.csv')
 
-df_classification['category'] = ""
+    df_classification = records_df.copy()
+    df_classification.drop_duplicates(inplace=True, ignore_index=True)
 
-df_classification = df_classification.query("assembly_number == 88")
+    df_classification['category'] = ""
 
-category_df = pd.read_csv('category.csv')
-category_df['full_text'] = category_df['Subtopic'] + ' ' + category_df['Topic'] + ', ' + category_df['Description'] 
-# print(category_df['full_text'])
+    df_classification = df_classification.query("assembly_number == 88")
 
-cat_dict = dict(zip(category_df.full_text, category_df.Code))
-categories = list(cat_dict.keys())
+    category_df = pd.read_csv('category.csv')
+    category_df['full_text'] = category_df['Subtopic'] + ' ' + category_df['Topic'] + ', ' + category_df['Description'] 
+    # print(category_df['full_text'])
 
-for i, row in df_classification.iterrows():
-    bill = str(row.Bill).replace(' ', '')
-    assy = str(row.assembly_number)
+    cat_dict = dict(zip(category_df.full_text, category_df.Code))
+    categories = list(cat_dict.keys())
 
-    file_path = os.path.join('Iowa', 'bills_text_' + assy, 'iowa_general_assembly_' + assy + '_' + bill + ".txt")
-    print(file_path)
+    for i, row in df_classification.iterrows():
+        bill = str(row.Bill).replace(' ', '')
+        assy = str(row.assembly_number)
 
-    with open(file_path, encoding="utf8") as f:
-        bill_text = f.read().replace('\n', ' ')
-    print(bill_text)
+        file_path = os.path.join('Iowa', 'bills_text_' + assy, 'iowa_general_assembly_' + assy + '_' + bill + ".txt")
+        
+        try:
+            with open(file_path, encoding="utf8") as f:
+                bill_text = f.read().replace('\n', ' ')
+                print(file_path)
+        except:
+            print("file not found:", file_path)
+            continue
 
-    extractor = CrossEncoderEventExtractor()
-    label = extractor.extract([bill_text], categories)[0]['events'][0]
+        extractor = CrossEncoderEventExtractor()
+        label = extractor.extract([bill_text], categories)[0]['events'][0]
 
 
-    df_classification.at[i,'category'] = cat_dict[label]
-    break
-    
-df_classification.to_csv('assy_88_classify.csv')
+        df_classification.at[i,'category'] = cat_dict[label]
+        df_classification.to_csv('assy_88_classify.csv', index=False)
 
+if __name__ == '__main__':
+    main()
