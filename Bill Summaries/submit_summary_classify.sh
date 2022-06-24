197a6842e997cdb5ba258a86705e00fae046f7fd
GZS <guozhaosengzs@yahoo.com>
2022-06-24 01:48:50 -0400

    Update submit_summary_classify.sh

--- Bill Summaries/submit_summary_classify.sh
+++ Bill Summaries/submit_summary_classify.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #$ -N bill_summary_classification
-#$ -l m_mem_free=5G
+#$ -l m_mem_free=10G
 #$ -j y
 #$ -m e
 
 module load python/python-3.9.6
 module load gcc/gcc-11.1.0
-source ../../../../venv396/bin/activate
-python -u web_scraping_copy.py
+source ../../venv396/bin/activate
+python -u summary_classify.py
